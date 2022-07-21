875efd4e55bf99a0715231a1e4f4adf768edb8aa
Diego Goulart <diego.goulart@edifyeducation.com.br>
2022-07-21 19:00:16 -0300

    feat: Create SimpleHeader component.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,11 @@
-import { HStack, IconButton, useTheme, VStack } from 'native-base';
-import { SignOut } from 'phosphor-react-native';
+import { Heading, HStack, IconButton, useTheme, StyledProps } from 'native-base';
+import { CaretLeft, SignOut } from 'phosphor-react-native';
 
 import Logo from "./../../../assets/logo_secondary.svg";
+
+export type SimpleHeaderProps = StyledProps & {
+  title: string;
+}
 
 export function Header() {
   const { colors } = useTheme();
@@ -24,6 +28,41 @@ export function Header() {
             bgColor: "gray.500"
           }
         } />
+    </HStack>
+  );
+}
+
+export function SimpleHeader({ title, ...rest }: SimpleHeaderProps) {
+  const { colors } = useTheme();
+
+  return (
+    <HStack
+      w="full"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="gray.600"
+      pt={12}
+      pb={6}
+      {...rest}
+    >
+      <IconButton
+        icon={<CaretLeft size={24} color={colors.gray[300]} />}
+        _pressed={
+          {
+            bgColor: "gray.500"
+          }
+        }
+      />
+
+      <Heading
+        color="gray.100"
+        textAlign="center"
+        fontSize="lg"
+        flex={1}
+        ml={-6}
+      >
+        {title}
+      </Heading>
     </HStack>
   );
 }
